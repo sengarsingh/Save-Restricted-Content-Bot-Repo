d347d8a99166f14c5e3952fc921f001f3f3c2203
sengarsingh <149374164+sengarsingh@users.noreply.github.com>
2024-07-04 08:47:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "25912801"))
+API_HASH = getenv("API_HASH", "f14e8717578935103e2774559184a345")
+BOT_TOKEN = getenv("BOT_TOKEN", "6728704718:AAHbgt-igQ0pfqderXjQtG2IdYNPQw-Wj7I")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1306706536").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://pranchal1419:<password>@cluster0.gicdshq.mongodb.net/")
+LOG_GROUP = getenv("LOG_GROUP", "-1002154249323")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002112434710"))
